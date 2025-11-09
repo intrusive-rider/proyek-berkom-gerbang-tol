e17419d567dadee76a53546fe152eb7424de18c5
farish <farishfe@proton.me>
2025-11-09 18:16:34 +0700

    Mengisolasi variabel 'faktor'

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ harga_per_gol = [20_000,30_000,40_000,50_000,60_000] # Harga per golongan
 
 # Perhitungan harga total
 tol_diff = abs(ruas_tol.index(tujuan_pengguna) - ruas_tol.index(asal_pengguna)) # "Selisih" ruas tol yang akan ditempuh
-harga_total = max(tol_diff + 1, 1) * harga_per_gol[gol_pengguna - 1] # Harga yang harus dibayar (bayar pas masuk + keluar [jika beda ruas])
+factor = max(tol_diff + 1, 1) # Pengali harga dari jarak yang ditempuh
+harga_total = factor * harga_per_gol[gol_pengguna - 1] # Harga yang harus dibayar (bayar pas masuk + keluar [jika beda ruas])
 
 saldo_cukup = False # Apakah saldo cukup
 
